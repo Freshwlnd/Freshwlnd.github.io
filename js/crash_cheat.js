52827fc1d73f833347af1e1eb75ece0882545aa6
freshwlnd <985605915@qq.com>
2020-10-23 17:46:38 +0800

    Site updated: 2020-10-23 17:46:37

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/TEP.ico");
-         document.title = '╭(°A°`)╮ 为什么不看我';
+         document.title = '╭(p.p`)╮ 你什么时候会来看我呢';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/favicon.ico");
-         document.title = '(ฅ>ω<*ฅ) 你怎么来了' + OriginTitle;
+         document.title = '(ฅ>ω<*ฅ) 你怎么来了！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
