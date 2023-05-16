0ee7cb52e2aad71c9c418fd7d9d292aa540275f1
freshwlnd <985605915@qq.com>
2023-05-17 01:26:03 +0800

    Site updated: 2023-05-17 01:26:03

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -1,17 +1,1 @@
-/*<!--崩溃欺骗-->*/
-var OriginTitle = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-    if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/img/TEP.ico");
-        document.title = '╮(˘､˘)╭ 记得你刚刚想看什么吗？';
-        clearTimeout(titleTime);
-    }
-    else {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
-        document.title = '¯\_(ツ)_/¯ 欢迎回来！' + OriginTitle;
-        titleTime = setTimeout(function () {
-            document.title = OriginTitle;
-        }, 2000);
-    }
-});
+var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",function(){document.hidden?($('[rel="icon"]').attr("href","/img/TEP.ico"),document.title="╮(˘､˘)╭ 记得你刚刚想看什么吗？",clearTimeout(titleTime)):($('[rel="icon"]').attr("href","/favicon.ico"),document.title="¯_(ツ)_/¯ 欢迎回来！"+OriginTitle,titleTime=setTimeout(function(){document.title=OriginTitle},2e3))});
